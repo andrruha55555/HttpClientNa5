dc07408d67ad20071c5513a565113415d5c50208
andruhaRR <andrushizy@yandex.ru>
2025-12-11 12:10:10 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace HttpClientNa5
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+        }
+    }
+}
